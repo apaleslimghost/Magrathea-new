d3f222e7f0fe3a2964024d245a284fbbd1683497
Matt Brennan <matt@153.io>
2017-08-16 19:58:24 +0100

    function

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const hexGrid = require('@quarterto/perturbed-hex-grid');
+const retinaCanvas = require('@quarterto/retina-canvas');
+const Loop = require('@quarterto/animation-loop');
 
 const {triangulate} = require('delaunay-fast');
-const c = document.createElement('canvas');
+const c = retinaCanvas();
 const ctx = c.getContext('2d');
 
+const dpr = window.devicePixelRatio;
+
 document.body.appendChild(c);
+document.body.style.margin = 0;
 
-c.width = c.height = 800;
-
-const grid = hexGrid({rows: 50, cols: 50 * Math.sqrt(3) / 2, size: 16});
-
-ctx.fillStyle = 'black';
-grid.forEach(([x, y]) => ctx.fillRect(x, y, 1, 1));
-
+const grid = hexGrid({rows: 50, cols: 50 * Math.sqrt(3) / 2, size: 20});
 const tris = triangulate(grid);
 
-for(let i = 0, l = tris.length; i < l; i += 3) {
-  const [x1, y1] = grid[tris[i]];
-  const [x2, y2] = grid[tris[i+1]];
-  const [x3, y3] = grid[tris[i+2]];
+const viewport = [[0, 0], [500, 500]];
 
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.lineTo(x3, y3);
-  ctx.lineTo(x1, y1);
-  ctx.stroke();
-}
+const drawPoints = () => grid.forEach(drawPoint);
+const drawPoint = ([x, y]) => ctx.fillRect(x, y, 1, 1);
+
+const drawTris = () => {
+	for(let i = 0, l = tris.length; i < l; i += 3) {
+		drawTri(
+			grid[tris[i]],
+			grid[tris[i+1]],
+			grid[tris[i+2]]
+		);
+	}
+};
+
+const drawTri = ([x1, y1], [x2, y2], [x3, y3]) => {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.lineTo(x3, y3);
+	ctx.lineTo(x1, y1);
+	ctx.stroke();
+};
+
+const loop = new Loop;
+
+loop.on('tick', () => {
+	ctx.fillStyle = 'black';
+
+	drawPoints();
+	drawTris();
+});
+
+loop.start();
