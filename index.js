57d27286611b9ae5960e7b13d10a4022065d084f
Matt Brennan <matt@153.io>
2017-08-16 22:56:35 +0100

    cameras and controls and lights

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const τ = require('three-js')();
+const τ = require('three-js')([
+	require('three-js/addons/OrbitControls.js'),
+]);
+
 const hexGrid = require('@quarterto/perturbed-hex-grid');
 const retinaCanvas = require('@quarterto/retina-canvas');
 const Loop = require('@quarterto/animation-loop');
@@ -10,56 +13,31 @@ const w = window.innerWidth;
 const h = window.innerHeight;
 
 const scene = new τ.Scene();
-const camera = new τ.OrthographicCamera(-w/2, w/2, h/2, -h/2, 0, 1000);
+// const camera = new τ.OrthographicCamera(-w/2, w/2, w/2, -w/2, 0, 1000);
+const camera = new τ.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+camera.position.z = 500;
+camera.position.y = 500;
 
 scene.add(camera);
 
 const renderer = new τ.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 document.body.style.margin = 0;
 
 const grid = hexGrid({rows: 50, cols: 50 * Math.sqrt(3) / 2, size: 20});
 const tris = triangulate(grid);
-const heights = grid.map(() => 4 * Math.random());
-
-// let viewport = [[0, 500], [0, 500]];
-//
-// const v = ([x, y]) => [
-// 	remap(viewport[0], [0, c.width])(x),
-// 	remap(viewport[1], [0, c.height])(y),
-// ];
-//
-// const drawPoints = () => grid.forEach(drawPoint);
-// const drawPoint = ([x, y]) => ctx.fillRect(...v([x, y]), 1, 1);
-//
-// const drawTris = () => {
-// 	for(let i = 0, l = tris.length; i < l; i += 3) {
-// 		drawTri(
-// 			grid[tris[i]],
-// 			grid[tris[i+1]],
-// 			grid[tris[i+2]]
-// 		);
-// 	}
-// };
-//
-// const drawTri = ([x1, y1], [x2, y2], [x3, y3]) => {
-// 	ctx.beginPath();
-// 	ctx.moveTo(...v([x1, y1]));
-// 	ctx.lineTo(...v([x2, y2]));
-// 	ctx.lineTo(...v([x3, y3]));
-// 	ctx.lineTo(...v([x1, y1]));
-// 	ctx.stroke();
-// };
+const heights = grid.map(() => Math.pow(3 * Math.random(), 2));
 
 const terrainGeom = new τ.Geometry();
 
 for(let i = 0, l = tris.length; i < l; i += 3) {
 	terrainGeom.vertices.push(
-		new τ.Vector3(grid[tris[i]][0],   heights[tris[i]],   grid[tris[i]][1]  ),
-		new τ.Vector3(grid[tris[i+1]][0], heights[tris[i+1]], grid[tris[i+1]][1]),
-		new τ.Vector3(grid[tris[i+2]][0], heights[tris[i+2]], grid[tris[i+2]][1])
+		new τ.Vector3(grid[tris[i]][0],  heights[tris[i]],    grid[tris[i]][1]  ),
+		new τ.Vector3(grid[tris[i+1]][0],heights[tris[i+1]],  grid[tris[i+1]][1]),
+		new τ.Vector3(grid[tris[i+2]][0],heights[tris[i+2]],  grid[tris[i+2]][1])
 	);
 
 	terrainGeom.faces.push(
@@ -70,50 +48,26 @@ for(let i = 0, l = tris.length; i < l; i += 3) {
 terrainGeom.computeFaceNormals();
 terrainGeom.computeVertexNormals();
 
-const material = new τ.MeshNormalMaterial();
+const material = new τ.MeshPhongMaterial({color: 0x006600});
 const terrain = new τ.Mesh( terrainGeom, material );
 scene.add(terrain);
 
-terrain.rotation.x = 90;
-terrain.position.x -= 400;
-terrain.position.y += 400;
+const directionalLight = new τ.DirectionalLight( 0xffffff );
+directionalLight.castShadow = true;
+scene.add( directionalLight );
 
-camera.position.z = 5;
+const box = new τ.Box3().setFromObject(terrain);
+
+const {x: terrainWidth, z: terrainHeight} = box.size();
+
+terrain.position.x = -terrainWidth/2;
+terrain.position.z = -terrainHeight/2;
+
+const controls = new τ.OrbitControls(camera, renderer.domElement);
 
 const loop = new Loop;
-
 loop.on('tick', () => {
 	renderer.render(scene, camera);
+	controls.update();
 });
-
 loop.start();
-
-// let dragging = false;
-// let startX, startY, startViewport;
-//
-// c.addEventListener('mousedown', ev => {
-// 	dragging = true;
-// 	startX = ev.clientX;
-// 	startY = ev.clientY;
-// 	startViewport = viewport;
-// });
-//
-// c.addEventListener('mouseup', () => {
-// 	dragging = false;
-// 	startX = startY = startViewport = null;
-// });
-//
-// c.addEventListener('mousemove', ev => {
-// 	if(dragging) {
-// 		viewport = [
-// 			[
-// 				startViewport[0][0] + startX - ev.clientX,
-// 				startViewport[0][1] + startX - ev.clientX,
-// 			],
-// 			[
-// 				startViewport[1][0] + startY - ev.clientY,
-// 				startViewport[1][1] + startY - ev.clientY,
-// 			],
-// 		];
-// 	}
-// });
